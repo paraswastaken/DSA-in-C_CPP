e96214955c8be4509971d4a7a4505b43b84a67fc
paras <akarkaulia2000@gmail.com>
2022-06-19 20:11:58 +0530

    BST traversal, invert BST implementation

--- bst.cpp
+++ bst.cpp
@@ -38,9 +38,80 @@ bool search(node* cNode, int dat){
     else return search(cNode->left, dat);
 }
 
+void inOrder(node* cNode){
+    if(cNode==NULL) return;
+    inOrder(cNode->left);
+    cout<<cNode->data<<" ";
+    inOrder(cNode->right);
+    return;
+}
+
+void preOrder(node* cNode){
+    if(cNode==NULL) return;
+    cout<<cNode->data<<" ";
+    preOrder(cNode->left);
+    preOrder(cNode->right);
+    return;
+}
+
+void postOrder(node* cNode){
+    if(cNode==NULL) return;
+    postOrder(cNode->left);
+    postOrder(cNode->right);
+    cout<<cNode->data<<" ";
+    return;
+}
+
+void invertBT(node* cNode){
+    if(cNode==NULL) return;
+    invertBT(cNode->left);
+    invertBT(cNode->right);
+    node* temp = cNode->left;
+    cNode->left = cNode->right;
+    cNode->right = temp;
+    return;
+}
+
 int main(){
-    root = insert(root, 15);
-    root = insert(root, 10);
-    root = insert(root, 21);
-    cout<<search(root, 21);
+//    root = insert(root, 15);
+//    root = insert(root, 10);
+//    root = insert(root, 21);
+    int sw, val;
+    do {
+        cout<<"Enter your choice: 1. Insert 2. Search 3. Print Inorder 4. Print Preorder 5. Print Postorder 6. Invert Tree 7. Exit"<<endl;
+        cin>>sw;
+        switch(sw){
+            case 1:
+                cout<<"Enter Value to enter:"<<endl;
+                cin>>val;
+                root=insert(root, val);
+                break;
+            case 2:
+                cout<<"Enter value to search:"<<endl;
+                cin>>val;
+                cout<<search(root, val)?"The value is present!":"Value is not present!!";
+                cout<<endl;
+                break;
+            case 3:
+                inOrder(root);
+                cout<<endl;
+                break;
+            case 4:
+                preOrder(root);
+                cout<<endl;
+                break;
+            case 5:
+                postOrder(root);
+                cout<<endl;
+                break;
+            case 6:
+                invertBT(root);
+                break;
+            case 7:
+                cout<<"Exiting!!!"<<endl;
+                break;
+            default:
+                cout<<"Enter valid option"<<endl;
+        }
+    }while(sw!=7);
 }
